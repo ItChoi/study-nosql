e41b6d41f1c6a80be4e0a5213d46f37b075fe2b5
최상현 <itchoi0429@gmail.com>
2024-01-27 10:48:36 +0900

    first commit

---
+++ src/main/java/com/example/studynosql/StudyNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.studynosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNosqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNosqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studynosql/StudyNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studynosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNosqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
